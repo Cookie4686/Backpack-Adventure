5259070ea764bc185897da4f6aa3894bfc4c6308
excursion <migth.np4@gmail.com>
2025-02-28 03:24:26 +0700

    Make mobs animate

--- src/entities/EntityLoader.java
+++ src/entities/EntityLoader.java
@@ -1,9 +1,14 @@
 package entities;
 
+import java.util.ArrayList;
 import java.util.function.Supplier;
 
-
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.util.Duration;
+import logic.handler.EntityHandler;
 
 public class EntityLoader {
 	public static Entity newEntity(String name) {
@@ -14,20 +19,45 @@ public class EntityLoader {
 
 class Resource {
 	private Supplier<Entity> supplier;
-	private String path;
-	private Image image;
+	private ArrayList<String> paths;
+	private ArrayList<Image> images;
 
-	public Resource(Supplier<Entity> supplier, String path) {
+	public Resource(Supplier<Entity> supplier, ArrayList<String> arrayList) {
 		this.supplier = supplier;
-		this.path = path;
+		this.paths = arrayList;
 	}
 
 	public Entity newEntity() {
-		if (image == null) {
-			image = new Image(ClassLoader.getSystemResource(path).toString());
+		if (images == null) {
+			images = new ArrayList<Image>();
+			for(String p : paths) {
+				Image image = new Image(ClassLoader.getSystemResource(p).toString());
+				if (image == null) {
+				    System.err.println("ERROR: Could not load image from " + p);
+				} else {
+				    System.out.println("Successfully loaded image from " + p);
+				}
+				images.add(image);
+			}
 		}
 		Entity entity = supplier.get();
-		entity.initialize(image);
+		ImageView imageView = entity.getImageView();
+		
+		Timeline timeline = new Timeline();
+		for (int i = 0; i < images.size(); i++) {
+	        final int frameIndex = i;
+	        KeyFrame keyFrame = new KeyFrame(
+	            Duration.seconds(0.2 * i),
+	            event -> imageView.setImage(images.get(frameIndex))
+	        );
+	        timeline.getKeyFrames().add(keyFrame);
+	    }
+		timeline.setCycleCount(Timeline.INDEFINITE);
+	    timeline.play();
+	    imageView.setImage(images.get(0));
+		imageView.setOnMousePressed(event -> EntityHandler.handleMouseClicked(entity));
+		entity.getChildren().add(imageView);
+		entity.setTimeline(timeline);
 		return entity;
 	}
 }
