e1c2a605d41a4f76992ca219d0844d83574d6427
Cookie4686 <cookieandcake1105@gmail.com>
2025-02-27 17:59:19 +0700

    change menu font

--- src/scene/MenuScene.java
+++ src/scene/MenuScene.java
@@ -4,6 +4,8 @@ import application.Main;
 import component.Button;
 import javafx.geometry.Pos;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import scene.popup.CharacterPopup;
 import scene.popup.SettingPopup;
@@ -18,7 +20,8 @@ public class MenuScene {
 		actionBox.setSpacing(20);
 		actionBox.setAlignment(Pos.CENTER);
 
-		Text titleText = new Text("Cool game");
+		Text titleText = new Text("Cool Game");
+		titleText.setFont(Font.font("Comic Sans MS", FontWeight.BOLD, 64));
 		Button startButton = new Button("Start", 128, 32);
 		Button settingButton = new Button("Settings", 128, 32);
 		startButton.setOnAction(_ -> CharacterPopup.getInstance().getPopup().show());
