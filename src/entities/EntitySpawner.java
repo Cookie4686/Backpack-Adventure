dc0f51e03fad40a2864964d300c9fef8fc1ea4d3
excursion <migth.np4@gmail.com>
2025-03-01 01:35:11 +0700

    add mob spawner

---
+++ src/entities/EntitySpawner.java
@@ -0,0 +1,42 @@
+package entities;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import game.util.MobTier;
+import logic.GameLogic;
+
+public class EntitySpawner {
+	private static ArrayList<String> e, d, c, b, a, s;
+	
+	private static MobTier getTier() {
+		//TODO: Connect luck and floor system
+		int luckNumber = new Random().nextInt(100) + 1 + GameLogic.getInstance().getCurrentFloor() * 90;
+		
+		if (luckNumber<=40) return MobTier.E;
+		if (luckNumber<=90) return MobTier.D;
+		if (luckNumber<=130) return MobTier.C;
+		if (luckNumber<=180) return MobTier.B;
+		if (luckNumber<=255) return MobTier.A;
+		return MobTier.S;
+	}
+	
+	private static String getNameFromTier(MobTier mobTier) {
+		switch (mobTier) {
+		case E:
+			return e.get(new Random().nextInt(e.size()));
+		case D:
+			return d.get(new Random().nextInt(d.size()));
+		case C:
+			return c.get(new Random().nextInt(c.size()));
+		case B:
+			return b.get(new Random().nextInt(b.size()));
+		case A:
+			return a.get(new Random().nextInt(a.size()));
+		case S:
+			return s.get(new Random().nextInt(s.size()));	
+		default:
+			return "";
+		}
+	}
+}
