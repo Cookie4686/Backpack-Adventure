f7a10e65cd09081606f83b54af296441a9a44a0d
excursion <migth.np4@gmail.com>
2025-03-01 07:58:31 +0700

    add more entity for testing

--- src/logic/GameLogic.java
+++ src/logic/GameLogic.java
@@ -22,6 +22,7 @@ public class GameLogic {
 		FightLogic.getInstance().getEntities().add(EntityLoader.newEntity("werewolf"));
 		FightLogic.getInstance().getEntities().add(EntityLoader.newEntity("ghost"));
 		FightLogic.getInstance().getEntities().add(EntityLoader.newEntity("flyingAlien"));
+		FightLogic.getInstance().getEntities().add(EntityLoader.newEntity("flyingAlien"));
 		FightLogic.getInstance().setTarget(FightLogic.getInstance().getEntities().getFirst());
 		GameBottom.getInstance().render();
 		//ThemeSongLoader.play("boss1");
