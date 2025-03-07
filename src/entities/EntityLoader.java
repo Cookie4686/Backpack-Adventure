ab4eb2d804df9942ac9aa53d7a75c4a96bc3bce4
Cookie4686 <cookieandcake1105@gmail.com>
2025-03-07 22:24:50 +0700

    fix entity handler complie error

--- src/entities/EntityLoader.java
+++ src/entities/EntityLoader.java
@@ -7,9 +7,7 @@ import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseButton;
 import javafx.util.Duration;
-import logic.handler.EntityHandler;
 
 public class EntityLoader {
 	public static Entity newEntity(String name) {
@@ -54,9 +52,6 @@ class Resource {
 		timeline.setCycleCount(Timeline.INDEFINITE);
 		timeline.play();
 		imageView.setImage(images.get(0));
-		imageView.setOnMousePressed(event -> {
-			if(event.getButton() == MouseButton.PRIMARY) EntityHandler.handleMouseClicked(entity);
-		});
 		entity.getChildren().add(imageView);
 		entity.setTimeline(timeline);
 		return entity;
