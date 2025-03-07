651bf7bfc38bf8786b3a5ecdb5282147c7c24757
Cookie4686 <cookieandcake1105@gmail.com>
2025-03-07 22:18:19 +0700

    make doctor passable in map

--- src/game/map/Map.java
+++ src/game/map/Map.java
@@ -155,8 +155,8 @@ public class Map extends GridPane {
 
 	private boolean recur(int x, int y, boolean[][] visited, Position targetPosition) {
 		switch (squares[x][y].getMarker()) {
-		case PLAYER, PATH	-> {}
-		default				-> { return x == targetPosition.getX() && y == targetPosition.getY(); }
+		case PLAYER, PATH, DOCTOR	-> {}
+		default						-> { return x == targetPosition.getX() && y == targetPosition.getY(); }
 		}
 
 		if (x + 1 != width && !visited[x + 1][y]) {
